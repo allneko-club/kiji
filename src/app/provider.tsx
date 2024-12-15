ddb79abf2bdb62c4d9a119d6231710e8cc24dfcd
Tanahashi <contrapunctus1to12@gmail.com>
2024-12-15 16:18:31 +0900

    * ReactQueryDevtoolsが表示されないバグを修正

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -24,7 +24,7 @@ export const AppProvider = ({ children }: AppProviderProps) => {
   return (
     <ErrorBoundary FallbackComponent={MainErrorFallback}>
       <QueryClientProvider client={queryClient}>
-        {process.env.DEV && <ReactQueryDevtools />}
+        {process.env.NODE_ENV === "development" && <ReactQueryDevtools />}
         {children}
         <ToastContainer autoClose={3000}/>
       </QueryClientProvider>
