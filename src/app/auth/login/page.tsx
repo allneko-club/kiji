fe72ecbec46310f5cafc12cd657a830864e58464
Tanahashi <contrapunctus1to12@gmail.com>
2024-12-15 13:44:19 +0900

    * ファイルを移動

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,12 +1,10 @@
 "use client"
-import LoginForm from '@/components/auth/login-form';
+import LoginForm from '@/app/auth/login/_components/login-form';
 
 export default function Page() {
   return (
-    <div>
-      <div className="flex h-screen w-full items-center justify-center px-4">
-        <LoginForm />
-      </div>
+    <div className="flex h-screen w-full items-center justify-center px-4">
+      <LoginForm />
     </div>
   );
 };
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,12 +1,10 @@
 "use client"
-import RegisterForm from '@/components/auth/register-form';
+import RegisterForm from '@/app/auth/register/_components/register-form';
 
 export default function Page() {
   return (
-    <div>
-      <div className="flex h-screen w-full items-center justify-center px-4">
-        <RegisterForm />
-      </div>
+    <div className="flex h-screen w-full items-center justify-center px-4">
+      <RegisterForm />
     </div>
   );
 };
